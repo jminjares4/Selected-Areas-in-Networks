3b512c4733dbaad554a9b082a2f5f6b92ca0c697
jminjares4 <minjares.jesus.ths@gmail.com>
2022-02-09 19:51:03 -0700

    Updated assignment2.py

--- assignment_2/assignment2.py
+++ assignment_2/assignment2.py
@@ -8,37 +8,44 @@ want to see a lot of detail in the histogram so do not use the default number of
 many more. Hint: use column='BYTES' as one of the arguments to the hist() function to 
 get it to work correctly.
 """
-# Data: 02-02-2022
+# Data: 02-09-2022
 
 # import pandas and matplotlib packages 
 import pandas as pd
 import matplotlib.pyplot as plt
+import sys
 
-# Store csv file into a data frame
-data = pd.read_csv('JesusMinjaresData.csv')
+if __name__=="__main__":
+    if len(sys.argv) < 2:
+        print('Usage: python3 assignment2.py file.csv')
+    else:
+        # store argument into variable
+        my_file = str(sys.argv[1])
 
-# get the min and max value for 'BYTES' column of the data frame
-min_val, max_val = min(data['BYTES']) , max(data['BYTES'])
+        # Store csv file into a data frame
+        data = pd.read_csv(my_file)
 
-# get the min and max value for the frequency of the packets
-y_min, y_max = min(data['BYTES'].value_counts()), max(data['BYTES'].value_counts())
+        # get the min and max value for 'BYTES' column of the data frame
+        min_val, max_val = min(data['BYTES']) , max(data['BYTES'])
 
-# create a histogram using pandas.DataFrame.hist for 'BYTES' with 400 bins 
-data_frame_graph = pd.DataFrame.hist(data, column='BYTES',grid=False,bins=400,range=[min_val,max_val],label='Bytes', xlabelsize=10, ylabelsize=10)
+        # get the min and max value for the frequency of the packets
+        y_min, y_max = min(data['BYTES'].value_counts()), max(data['BYTES'].value_counts())
 
-plt.title('BYTES', fontweight='bold')
+        # create a histogram using pandas.DataFrame.hist for 'BYTES' with 400 bins 
+        data_frame_graph = pd.DataFrame.hist(data, column='BYTES',grid=False,bins=400,range=[min_val,max_val],
+                                        label='Bytes', xlabelsize=10, ylabelsize=10, color='midnightblue')
+        # Add title
+        plt.title('Histogram of Bytes in NetFlow', fontweight='bold')
 
-# set xlabel
-plt.xlabel('Packets (BYTES)')
-# set ylabel
-plt.ylabel('Frequency')
-# display legend
-plt.legend()
-# add text @ the middle of the histogram
-plt.text(max_val/2, y_max/2,f'Min: {min_val}\nMax: {max_val}')
-# save figure
-plt.savefig('assignment_2_hist.png')
-# display graph
-plt.show() 
-
-
+        # set xlabel
+        plt.xlabel('Packets (BYTES)')
+        # set ylabel
+        plt.ylabel('Frequency')
+        # display legend
+        plt.legend()
+        # add text @ the middle of the histogram
+        plt.text(max_val/2, y_max/2,f'Min: {min_val}\nMax: {max_val}')
+        # save figure
+        plt.savefig('assignment_2_hist.png')
+        # display graph
+        plt.show()
