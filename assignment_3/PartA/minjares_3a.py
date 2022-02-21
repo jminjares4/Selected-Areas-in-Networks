4425054fcdc85d4aa3cd4afd35f24cee7445b0b2
jminjares4 <minjares.jesus.ths@gmail.com>
2022-02-21 15:21:09 -0700

    Added .py files

---
+++ assignment_3/PartA/minjares_3a.py
@@ -0,0 +1,15 @@
+# Author: Jesus Minjares
+# Assignment 3A:
+"""
+  Capture flow data with pmacct to a CSV file. Write a python script that loads the flow 
+  data into a PANDAS dataframe. Create a new dataframe that is a subset of the original 
+  data frame using PANDAS to isolate the TCP flows where the number of flow bytes 
+  exceeds 100. Submit this python script through Blackboard named lastname_3a.py. 
+"""
+# Data: 02-21-2022
+
+import pandas as pd
+import sys
+
+if __name__=="__main__":
+    print("TODO: assigment 3A")
---
+++ assignment_3/PartB/minjares_3b.py
@@ -0,0 +1,14 @@
+# Author: Jesus Minjares
+# Assignment 3B:
+"""
+    Add four colums to your original PANDAS dataframe from 4) called 'src_as', 'src_org', 
+  'dst_as', 'dst_org' using the data in the 'as.csv' file. Submit your Python script through 
+  Blackboard named lastname_3b.py.  
+"""
+# Data: 02-21-2022
+
+import pandas as pd
+import sys
+
+if __name__=="__main__":
+    print("TODO: assignment 3B")
